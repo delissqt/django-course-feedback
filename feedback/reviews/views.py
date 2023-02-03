4a982e6e987c1f9dbf44fc73ddfa5d31f5807e77
delia gt <delissqt@gmail.com>
2023-02-03 17:43:45 -0600

    Using Session Data

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -45,11 +45,18 @@ class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
     model = Review
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        loaded_review = self.object 
+        request = self.request
+        favorite_id = request.session["favorite_review"]
+        context["is_favorite"] = favorite_id == str(loaded_review.id)
+        return context
+
 
 class AddFavoriteView(View):
     def post(self, request):
         review_id = request.POST["review_id"]
-        #favorite_review = Review.objects.get(pk=review_id)
         print("REVIEW_ID", review_id)
         request.session["favorite_review"] = review_id
         return HttpResponseRedirect("/reviews/" + review_id)
