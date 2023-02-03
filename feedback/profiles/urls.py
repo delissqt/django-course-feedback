363d6b0956cb93d4c3c91e4256df42a51c90d053
delia gt <delissqt@gmail.com>
2023-02-03 12:47:45 -0600

    Working with the FieldField
    Still in procees display images in /profiles/list

--- feedback/profiles/urls.py
+++ feedback/profiles/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.CreateProfileView.as_view()),
+    path("list", views.ProfilesView.as_view()),
 ]
--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 
 from .forms import ProfileForm
 from .models import UserProfile
@@ -13,3 +14,10 @@ class CreateProfileView(CreateView):
     model = UserProfile
     fields = "__all__"
     success_url = "/profiles"
+
+
+class ProfilesView(ListView):
+    template_name = "profiles/user_profiles.html"
+    model = UserProfile
+    context_object_name = "profiles"
+    
