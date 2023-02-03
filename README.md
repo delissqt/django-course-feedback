fb1367a681e486b92fc35694c0d64c916623e865
delia gt <delissqt@gmail.com>
2023-02-03 12:20:23 -0600

    Using a CreateView

--- README.md
+++ README.md
@@ -513,3 +513,44 @@ class UserProfile(models.Model):
 class ProfileForm(forms.Form):
     user_image = forms.ImageField()
 ```
+
+---
+
+# CreateView
+
+using `CreateView`
+
+```
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
+```
+
+
+**without** using `CreateView`
+```
+class CreateProfileView(View):
+    def get(self, request):
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {
+            "form": form
+        })
+
+    
+    def post(self, request):
+        #request.FILES["image"]
+        submitted_form = ProfileForm(request.POST, request.FILES)
+
+        if submitted_form.is_valid():
+            profile = UserProfile(image=request.FILES["user_image"])
+            profile.save()
+            #store_file(request.FILES["image"])
+            return HttpResponseRedirect("/profiles")
+        
+        return render(request, "profiles/create_profile.html", {
+            "form": submitted_form
+        })
+
+```
--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,36 +1,15 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView
 
 from .forms import ProfileForm
 from .models import UserProfile
 
 # Create your views here.
 
-#def store_file(file):
-#    with open("temp/image.jpg", "wb+") as destination:
-#        for chunk in file.chunks():
-#            destination.write(chunk)
-
-
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {
-            "form": form
-        })
-
-    
-    def post(self, request):
-        #request.FILES["image"]
-        submitted_form = ProfileForm(request.POST, request.FILES)
-
-        if submitted_form.is_valid():
-            profile = UserProfile(image=request.FILES["user_image"])
-            profile.save()
-            #store_file(request.FILES["image"])
-            return HttpResponseRedirect("/profiles")
-        
-        return render(request, "profiles/create_profile.html", {
-            "form": submitted_form
-        })
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
